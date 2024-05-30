70af39ef88b279e26d94a37a4b6383de85cdf6cc
bangueco <121280104+bangueco@users.noreply.github.com>
2024-05-30 10:46:35 +0800

    Add validation messages

---
+++ validation_message.py
@@ -0,0 +1,14 @@
+def display_marker_validation_msg():
+  print('Please enter a valid marker (x/o)')
+
+def display_invalid_input_msg():
+  print('Please enter a valid input')
+
+def display_name_validation_msg():
+  print('Please enter a valid name')
+
+def display_name_taken_msg():
+  print('Name is already taken')
+
+def display_marker_taken_msg():
+  print('Marker is already taken!')
