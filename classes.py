be52e205c670f891afba59d726e1a3a3dfc4db05
bangueco <121280104+bangueco@users.noreply.github.com>
2024-05-31 09:48:31 +0800

    feat: add place marker functionality to object

--- classes.py
+++ classes.py
@@ -1,4 +1,9 @@
+from logic import Game
 class Player:
   def __init__(self, name:str, marker:str):
     self.name = name
     self.marker = marker
+
+  def place_marker(self, position):
+    Game.gameboard[position] = self.marker
+    return
